76936b23458b977b6de9eee8f211bfef74d6c94f
Anton <goncharov@inbox.ru>
2021-08-16 15:27:26 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path'); 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+      test: /\.m?js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            ['@babel/preset-env', { targets: "defaults" }]
+          ]
+        }
+      }
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/inline',
+        // use: 'file-loader',
+      },
+    ],
+  },
+  devServer: {
+    open: true,
+    compress: true,
+    port: 4000,
+   },
+};
+
